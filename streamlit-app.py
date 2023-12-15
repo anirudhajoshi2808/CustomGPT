e3bc1ae0edf4dea2a1860922b425077f2c0e1dc2
anirudhajoshi2808 <joshi.anir@northeastern.edu>
2023-12-14 20:08:35 -0500

    updated the streamlit code

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,28 +1,24 @@
 import streamlit as st
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import fitz  # PyMuPDF
 
-# Load GPT-2 model and tokenizer
 model_name = "gpt2"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# List of prompts
 prompts = [
-    "Apply the RBF kernel SVM to the Breast Cancer dataset",
     "What is overfitting?",
+    "What is underfitting?",
     "What are the advantages of using neural networks for image recognition?",
-    # Add more prompts as needed
+    "Apply the RBF kernel SVM to the Breast Cancer dataset"
 ]
 
-# Function to generate responses based on the selected prompt
-def generate_responses(prompt):
+def generate_response(prompt, num_responses=1):
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
 
     output = model.generate(
         input_ids,
         max_length=150,
-        num_return_sequences=1,
+        num_return_sequences=num_responses,
         no_repeat_ngram_size=2,
         top_k=50,
         top_p=0.95,
@@ -32,43 +28,25 @@ def generate_responses(prompt):
     )
 
     decoded_output = [tokenizer.decode(ids, skip_special_tokens=True) for ids in output]
-
     return decoded_output
 
-# Streamlit app
-st.title("Prompt Generator for Data Science Engg Tools and Methods")
-st.write("This Streamlit app generates responses based on prompts related to O'Reilly's Introduction to Machine Learning with Python book.")
-st.image("book.png", caption="Cover Photo", use_column_width=True)
+def main():
+    st.title("Prompt Generator for Data Science Engg Tools and Methods")
+    st.write("This Streamlit app generates responses based on prompts related to O'Reilly's Introduction to Machine Learning with Python book.")
+    st.image("book.png", caption="Cover Photo", use_column_width=True)
 
+    selected_starter = st.selectbox("Select a Conversation Starter", prompts)
 
-# Dropdown for selecting prompts
-selected_prompt = st.selectbox("Select a prompt:", prompts)
+    starter_responses = generate_response(selected_starter, num_responses=1)
+    st.write("Model: {}".format(starter_responses[0]))
 
-# Generate and display responses
-generated_responses = generate_responses(selected_prompt)
+    if st.button("Why do you think that?"):
+        follow_up_responses = generate_response("Why do you think that?", num_responses=1)
+        st.text("Model: {}".format(follow_up_responses[0]))
 
-st.markdown(f"**Prompt:** {selected_prompt}")
-st.markdown("**Generated Responses:**")
-for response in generated_responses:
-    st.write(response)
-    st.markdown("---")
+    if st.button("Be more specific"):
+        follow_up_responses = generate_response("Be more specific", num_responses=1)
+        st.text("Model: {}".format(follow_up_responses[0]))
 
-
-# st.title("Prompt Generator for Data Science Engg Tools and Methods")
-# st.image("book.png", caption="Cover Photo", use_column_width=True)
-
-# with st.container():
-    
-#     st.write("This Streamlit app generates responses based on prompts related to O'Reilly's Introduction to Machine Learning with Python book.")
-
-#     st.header("Select a Prompt")
-    
-#     selected_prompt = st.selectbox("Choose a prompt:", prompts)
-
-#     generated_responses = generate_responses(selected_prompt)
-
-#     st.subheader(f"Selected Prompt: {selected_prompt}")
-#     st.subheader("Generated Responses:")
-#     for response in generated_responses:
-#         st.write(response)
-#         st.markdown("---")
+if __name__ == "__main__":
+    main()
